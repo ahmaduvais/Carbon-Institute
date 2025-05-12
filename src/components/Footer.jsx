a0ed59af95d5ce7ef21c84f3177111f95a58d409
Shafaq <mohdshafaq25@gmail.com>
2025-05-13 03:03:24 +0530

    Updated instagram, facebook links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -50,7 +50,7 @@ const Footer = () => {
             <a href="#" className="hover:text-gray-300">
               <Twitter size={24} />
             </a>
-            <a href="https://www.instagram.com/carbon.institute/" className="hover:text-gray-300" target="_blank">
+            <a href="instagram.com/carbon.institute/" className="hover:text-gray-300" target="_blank">
               <Instagram size={24} />
             </a>
           </div>
